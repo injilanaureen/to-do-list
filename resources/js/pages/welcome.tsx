cbc807f6d6199dcd64bea763644ba858ef17890a
Your Name <injilanaureen@gmail.com>
2025-04-03 07:25:11 +0530

    all

---
+++ resources/js/pages/welcome.tsx
@@ -0,0 +1,16 @@
+
+export default function Welcome() {
+
+    return (
+        <div className='max-w-200 border-2 p-3 flex mx-auto my-20'>
+            
+           <div className='flex gap-2'>
+            <input type="checkbox" name='showall'/>
+            <h3>Show All Tasks</h3>
+           </div>
+
+           <div></div>
+               
+        </div>
+    );
+}
